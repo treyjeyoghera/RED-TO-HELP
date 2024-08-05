00dc77a27775ea4eba2ceb88e16a738bf542a21e
rogers.machara <rogersmachara13@gmail.com>
2024-08-05 12:05:04 +0300

    categories model

--- server/models.py
+++ server/models.py
@@ -4,3 +4,17 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin  # Import UserMixin
 
 db = SQLAlchemy()
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    # Relationships
+    employments = db.relationship('Employment', back_populates='category', lazy=True)
+    social_integrations = db.relationship('SocialIntegration', back_populates='category', lazy=True)
+    creator = db.relationship('User', back_populates='categories', lazy=True)
