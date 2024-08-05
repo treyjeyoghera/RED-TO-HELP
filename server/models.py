570ddf43a040ea3d5e76eb430bc1f19f1e2e9cad
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-08-05 11:37:11 +0300

    user

--- server/models.py
+++ server/models.py
@@ -4,3 +4,32 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin  # Import UserMixin
 
 db = SQLAlchemy()
+
+class User(db.Model, UserMixin):  # Inherit from UserMixin
+    __tablename__ = 'user'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    profile_picture = db.Column(db.String)
+
+    # Relationships
+    employments = db.relationship('Employment', back_populates='user', lazy=True)
+    applications = db.relationship('Application', back_populates='user', lazy=True)
+    categories = db.relationship('Category', back_populates='creator', lazy=True)
+    social_integrations = db.relationship('SocialIntegration', back_populates='user', lazy=True)
+
+    @property
+    def is_active(self):
+        return True  
+
+    @property
+    def is_authenticated(self):
+        return True  
+
+    @property
+    def is_anonymous(self):
+        return False  
