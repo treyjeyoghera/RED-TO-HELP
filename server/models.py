170b2bf7aaad95e10bc4df48a5aaa5c3ed87b41b
rogers.machara <rogersmachara13@gmail.com>
2024-08-05 12:45:19 +0300

    reentered all models

--- server/models.py
+++ server/models.py
@@ -1,20 +1,83 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Text, ForeignKey
 from sqlalchemy.orm import relationship
-from flask_login import UserMixin  # Import UserMixin
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
+class User(db.Model, UserMixin):  # Inherit from UserMixin
+    tablename = 'user'
 
-class Category(db.Model):
-    __tablename__ = 'category'
-    
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    description = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    profile_picture = db.Column(db.String)
 
-    # Relationships
-    employments = db.relationship('Employment', back_populates='category', lazy=True)
-    social_integrations = db.relationship('SocialIntegration', back_populates='category', lazy=True)
-    creator = db.relationship('User', back_populates='categories', lazy=True)
+# Relationships
+employments = db.relationship('Employment', back_populates='user', lazy=True)
+applications = db.relationship('Application', back_populates='user', lazy=True)
+categories = db.relationship('Category', back_populates='creator', lazy=True)
+social_integrations = db.relationship('SocialIntegration', back_populates='user', lazy=True)
+
+@property
+def is_active(self):
+    return True  
+
+@property
+def is_authenticated(self):
+    return True  
+
+@property
+def is_anonymous(self):
+    return False  
+class Employment(db.Model):
+    tablename = 'employment'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    requirements = db.Column(db.Text)
+    location = db.Column(db.String)
+    salary_range = db.Column(db.Integer)
+
+# Relationships
+user = db.relationship('User', back_populates='employments')
+category = db.relationship('Category', back_populates='employments', lazy=True)
+applications = db.relationship('Application', back_populates='employment', lazy=True)
+
+@staticmethod
+def create(user_id, category_id, title, description, requirements=None, location=None, salary_range=None):
+    employment = Employment(
+        user_id=user_id,
+        category_id=category_id,
+        title=title,
+        description=description,
+        requirements=requirements,
+        location=location,
+        salary_range=salary_range
+    )
+    db.session.add(employment)
+    db.session.commit()
+    return employment
+
+@staticmethod
+def get_all():
+    return Employment.query.all()
+
+@staticmethod
+def get_by_id(id):
+    return Employment.query.get(id)
+
+def update(self, **kwargs):
+    for key, value in kwargs.items():
+        setattr(self, key, value)
+    db.session.commit()
+
+def delete(self):
+    db.session.delete(self)
+    db.session.commit()
